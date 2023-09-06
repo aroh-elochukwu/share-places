c7c893f9a027ddc6e18368a59852b93fdee50223
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-09-06 14:23:59 -0500

    fix classname naming error in Avatar n Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Users from './users/pages/Users';
+import UserItem from "./users/components/UserItem";
 
 function App() {
   return (
-    <div>
-      <Users/>
-      <h2>zdgfhtjyr</h2>
-
-    </div>
+    <Router>
+      <Routes>
+        
+        <Route element={<Users/>} path='/' />     
+        <Route element={<UserItem/>} path='/user-item' />     
+        
+      </Routes>
+    </Router>
     
 
   );
