881b82d246003d1057c2af8e3b8ae4ada12ba31b
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-09-07 11:03:56 -0500

    plug in Card into User-item

--- frontend/src/users/components/UserItem.jsx
+++ frontend/src/users/components/UserItem.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Avatar from '../../shared/components/uiElements/Avatar'
+import Card from '../../shared/components/uiElements/Card';
 import styles from './UserItem.module.css'
 
 const UserItem = props => {
   return ( 
     <li className={styles['user-item']}> 
       <div className={styles['user-item__content']}>
-        <div className={styles['user-item__image']}>
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className={styles['user-item__info']}>
-          <h2>{props.name}</h2>
-          <h3>{props.placeCount}{props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-        </div>
+        <Card>
+          <Link to={`/${props.id}`}>
+            <div className={styles['user-item__image']}>
+              <Avatar image={props.image} alt={props.name} />
+            </div>
+            <div className={styles['user-item__info']}>
+              <h2>{props.name}</h2>
+              <h3>{props.placeCount}{props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+            </div>
+          </Link>
+        </Card>
       </div>
     </li>
   );
