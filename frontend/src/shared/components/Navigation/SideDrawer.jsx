994ab3b27b745a08a3bf7a9967a1839f051b94b0
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-09-07 17:38:54 -0500

    arrange SideDrawer on DOM with portal

--- frontend/src/shared/components/Navigation/SideDrawer.jsx
+++ frontend/src/shared/components/Navigation/SideDrawer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
 import styles from './SideDrawer.module.css';
 
 
 const SideDrawer = props => {
+  const content = <aside className={styles['side-drawer']}> {props.children} </aside>;
+
+
   return ( 
-    <aside className={styles['side-drawer']}>
-      {props.children}
-    </aside>
+    createPortal(content, document.getElementById('drawer-hook'))
    );
 }
  
